9d9cda18a19e9d39d425c62ae1c93941e33fd533
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-03-19 19:07:05 +0600

    api key added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Watch', description: 'running watch', price: '$99' },
@@ -8,16 +9,16 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <Grid conatiner justify="center" spacing={4}>
-                {products.map((product) => {
-                    return (
-                        <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-                            <Product product={product}/>
-                        </Grid>
-                    )
-                })}
+        <div className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
             </Grid>
         </div>
     );
